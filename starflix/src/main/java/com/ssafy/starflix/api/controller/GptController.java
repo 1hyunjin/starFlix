ebf686ecde7176e3a32cd77181a9b667aa0ba6e8
유현진 <a92046503@gmail.com>
2024-05-22 15:51:25 +0900

    add swagger description

--- starflix/src/main/java/com/ssafy/starflix/api/controller/GptController.java
+++ starflix/src/main/java/com/ssafy/starflix/api/controller/GptController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +15,13 @@ import com.ssafy.starflix.api.model.service.GptService;
 import com.ssafy.starflix.review.model.dto.ReviewDTO;
 import com.ssafy.starflix.review.model.service.ReviewService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/gpt")
+@CrossOrigin("*")
+@Tag(name = "gpt 컨트롤러", description = "gpt 리뷰 분석 controller")
 public class GptController {
 	
 	@Autowired
@@ -24,6 +30,7 @@ public class GptController {
 	@Autowired
 	private ReviewService rservice; 
 	
+	@Operation(summary="gpt가 해당 명소에 대한 리뷰 분석 실행")
 	@GetMapping("/reviews")
 	public ResponseEntity<?> generateReviews(@RequestParam int idx) throws Exception{
 		List<ReviewDTO> reviewList = rservice.gptList(idx);
