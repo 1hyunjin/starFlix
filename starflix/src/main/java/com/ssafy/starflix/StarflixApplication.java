7421e5d81796925e5b723c564ae26b0c5dd2ecfc
유현진 <a92046503@gmail.com>
2024-05-22 15:54:01 +0900

    add scheduling setting

--- starflix/src/main/java/com/ssafy/starflix/StarflixApplication.java
+++ starflix/src/main/java/com/ssafy/starflix/StarflixApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.starflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StarflixApplication {
 
 	public static void main(String[] args) {
